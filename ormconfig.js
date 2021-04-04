862c4f43f87485cc616272acc247dda66495d0d8
Fernando Corrêa da Silva <fernando_silvasc@hotmail.com>
2021-04-03 21:11:45 -0700

    changed ormconfig file

--- ormconfig.js
+++ ormconfig.js
@@ -4,7 +4,6 @@ module.exports = {
   entities: [`${process.env.ENTITIES_PATH}`],
   migrations: [`${process.env.MIGRATIONS_PATH}`],
   synchronize: false,
-  ssl: false,
   logging: true,
   cli: {
     migrationsDir: "./src/database/migrations",
